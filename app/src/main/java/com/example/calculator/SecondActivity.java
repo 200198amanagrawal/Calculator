b9feaeb99351492e1217affcc4baa1bdd108c1bf
Aman Agrawal <amanagrawal2098@gmail.com>
2020-05-23 00:14:06 +0530

    implicit intent to share some useful data

--- app/src/main/java/com/example/calculator/SecondActivity.java
+++ app/src/main/java/com/example/calculator/SecondActivity.java
@@ -4,19 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView textView;
     String data;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         textView=findViewById(R.id.textresult);
-        Intent intent=getIntent();
+        final Intent intent=getIntent();
         data=intent.getStringExtra("data");
-        textView.setText("Result bro "+data);
+        textView.setText("ALL you have to do is to catch that damn train CJ: "+data);
+        button=findViewById(R.id.share);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent();
+                intent1.setAction(Intent.ACTION_SEND);
+                intent1.setType("text/plain");
+                intent1.putExtra(Intent.EXTRA_TEXT,data);
+                startActivity(intent1);
+            }
+        });
     }
 }
